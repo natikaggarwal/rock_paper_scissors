ae533e986fcafcf25acb90c33a971437868fc2ac
Natik Aggarwal <emailnatikaggarwal@gmail.com>
2022-08-29 10:56:38 -0700

    Add logic to the game for computer selection

---
+++ game.js
@@ -0,0 +1,12 @@
+function getComputerChoice(){
+    const choices = ["Rock","Paper","Scissors"];
+    const random = Math.floor((Math.random()*choices.length));
+    return choices[random];
+}
+
+function playRound(playerChoice, computerChoice){
+    
+}
+
+const playerChoice = "rock";
+const computerChoice = console.log(getComputerChoice());
